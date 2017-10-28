bdf57def385fb39e82df11032f274474f7a7e1a4
FangBaoGui <995635324@qq.com>
2017-10-28 14:09:44 +0800

    所有

---
+++ consumerBygit/src/main/java/com/xgz/consumer/UserServiceConsumer.java
@@ -0,0 +1,24 @@
+package com.xgz.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.xgz.service.IUserService;
+
+/**
+ * Created by chanshuyi on 2016/1/19.
+ */
+@Component
+public class UserServiceConsumer {
+
+    private static Logger logger = LoggerFactory.getLogger(UserServiceConsumer.class);
+
+    public static void main(String args[]) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        IUserService userService = (IUserService) ctx.getBean("userService");
+        logger.info("执行结果：" + userService.login("hello", "hello"));
+    }
+}
---
+++ providerBygit/src/main/java/com/xgz/service/Impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.xgz.service.Impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.xgz.service.IUserService;
+
+@Service("userService")
+public class UserServiceImpl implements IUserService {
+
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    public boolean login(String username, String password) {
+        logger.info("用户登录：[username:{}, password:{}]", username, password);
+        if (username != null && password != null && username.equals(password)) {
+            logger.info("用户校验通过。[username:{}]", username);
+            return true;
+        }
+        logger.info("用户校验失败！[username:{}]", username);
+        return false;
+    }
+
+}
